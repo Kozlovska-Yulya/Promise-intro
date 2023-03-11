c3948aa843b59577f4f9788e6e3477cb218d5cdf
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-03-11 23:22:42 +0100

    added function promise

---
+++ Task-2/index.js
@@ -0,0 +1,29 @@
+import { addImage } from './addImage.js';
+
+const addImageV2 = (url) => {
+  const promise = new Promise((resolve, reject) => {
+    const img = document.createElement('img');
+    img.setAttribute('alt', 'User avatar');
+    img.src = url;
+
+    const pageElem = document.querySelector('.page');
+    pageElem.append(img);
+
+    const onImageLoaded = () => {
+      const { width, height } = img;
+      resolve({ width, height });
+    };
+    const onImageLoadError = () => reject('Image load failed');
+
+    img.addEventListener('load', onImageLoaded);
+
+    img.addEventListener('error', onImageLoadError);
+  });
+  return promise;
+};
+
+const img = 'https://server.com/image.png';
+
+const resultPromise = addImageV2(img);
+
+resultPromise.then((data) => console.log(data));
